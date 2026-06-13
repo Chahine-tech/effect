c21c106ebfabccf5c38d636e36d606b59dd21a5f
Chahine-tech <chahine.benlahcen@gmail.com>
2026-06-13 13:31:36 +0200

    refactor: rename eventsEffect to makeEventsEffect and improve structure

--- packages/api/src/interface/handlers/events.handler.ts
+++ packages/api/src/interface/handlers/events.handler.ts
@@ -1,39 +1,41 @@
 import { HttpApiBuilder, HttpServerResponse } from "@effect/platform"
 import { Effect, PubSub, Schedule, Stream } from "effect"
-import { UserEventBus } from "../../infrastructure/events.js"
+import { UserEvent, UserEventBus } from "../../infrastructure/events.js"
 
 const encoder = new TextEncoder()
 
-const eventsEffect = Effect.gen(function* () {
-  const bus = yield* UserEventBus
+const makeEventsEffect = (bus: PubSub.PubSub<UserEvent>) =>
+  Effect.gen(function* () {
+    const events = Stream.unwrapScoped(
+      Effect.gen(function* () {
+        const queue = yield* PubSub.subscribe(bus)
+        return Stream.fromQueue(queue).pipe(
+          Stream.map((event) => {
+            const msg = `event: ${event._tag}\ndata: ${JSON.stringify(event)}\n\n`
+            return encoder.encode(msg)
+          })
+        )
+      })
+    )
 
-  const events = Stream.unwrapScoped(
-    Effect.gen(function* () {
-      const queue = yield* PubSub.subscribe(bus)
-      return Stream.fromQueue(queue).pipe(
-        Stream.map((event) => {
-          const msg = `event: ${event._tag}\ndata: ${JSON.stringify(event)}\n\n`
-          return encoder.encode(msg)
-        })
-      )
+    // Send a comment every 30s to keep the connection alive through proxies
+    const keepalive = Stream.fromSchedule(Schedule.fixed("30 seconds")).pipe(
+      Stream.as(encoder.encode(": keepalive\n\n"))
+    )
+
+    return HttpServerResponse.stream(Stream.merge(events, keepalive), {
+      headers: {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        "Connection": "keep-alive",
+        "X-Accel-Buffering": "no",
+      },
     })
-  )
-
-  // Send a comment every 30s to keep the connection alive through proxies
-  const keepalive = Stream.fromSchedule(Schedule.fixed("30 seconds")).pipe(
-    Stream.as(encoder.encode(": keepalive\n\n"))
-  )
-
-  return HttpServerResponse.stream(Stream.merge(events, keepalive), {
-    headers: {
-      "Content-Type": "text/event-stream",
-      "Cache-Control": "no-cache",
-      "Connection": "keep-alive",
-      "X-Accel-Buffering": "no",
-    },
   })
-})
 
 export const EventsHandlerLive = HttpApiBuilder.Router.use((router) =>
-  router.get("/events", eventsEffect)
+  Effect.gen(function* () {
+    const bus = yield* UserEventBus
+    yield* router.get("/events", makeEventsEffect(bus))
+  })
 )
